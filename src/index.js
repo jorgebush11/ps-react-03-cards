99801f49da615a6b3c3162b8785da631b40e49ed
jorgebush11 <70361438+jorgebush11@users.noreply.github.com>
2020-10-16 08:23:43 -0500

    profile design styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
 
 import "./styles.scss";
 
-ReactDom.render(
-	React.createElement("h1", {}, "GitHub Cards App"),
-	document.querySelector("#root")
-);
+const root = document.querySelector("#root");
+
+class Card extends Component {
+	render() {
+		return (
+			<div className="profile">
+				<img src="https://placehold.it/75" alt="" />
+				<div className="info">
+					<div className="name">Name here...</div>
+					<div className="company">Company here...</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="header">{this.props.title}</h1>
+				<Card />
+			</div>
+		);
+	}
+}
+
+ReactDom.render(<App title="The GitHub Cards App" />, root);
